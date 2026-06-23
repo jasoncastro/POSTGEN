7ca047b63994312ada5771122409bccb33b9e842
Jason Christopher Castro <jasoncastro15@gmail.com>
2026-06-23 10:57:19 +0800

    Change base path in Vite config to '/POSTGEN/'

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     //base: process.env.VITE_BASE || '/',
-    base: process.env.VITE_BASE || '/',
+    base: process.env.VITE_BASE || '/POSTGEN/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
